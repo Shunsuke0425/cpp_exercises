9a6357dcd4e0645de7337f8126aafdadd86bc07c
Shunsuke0425 <migihawkssince2000@gmail.com>
2021-10-21 12:07:20 +0900

    I have done the quiz02 Stream problem!!

---
+++ ex02/test_stream.cpp
@@ -0,0 +1,34 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include "stack.cpp"
+using namespace std;
+int main(int argc, char *argv[]){
+    ifstream readfile("Data.txt");
+    if(!readfile){
+        cerr << "Cannot read!" << endl;
+        exit(1);
+    }
+    Stack stack;
+    string str;
+    readfile >> str;
+    int max_number = stoi(str);
+    int max_length = 0;
+    for(int i = 0; i < max_number; i++){
+        readfile >> str;
+        stack.push(str);
+        if(max_length < str.length())max_length = str.length();
+    }
+    for(int i = 0; i < max_length; i++){
+        cout << "-";
+    }
+    cout << "-" << endl;
+    for(int i = 0; i < max_number; i++){
+        cout << stack.top() << endl;
+        stack.pop();
+    }
+    for(int i = 0; i < max_length; i++){
+        cout << "-";
+    }
+    cout << "-" << endl;
+}
