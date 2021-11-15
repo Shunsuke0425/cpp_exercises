f459d2202400021bed10627e99ac620d63704adc
Shunsuke0425 <migihawkssince2000@gmail.com>
2021-11-15 11:26:59 +0900

    Modify a little bit

--- ex04/Date.cpp
+++ ex04/Date.cpp
@@ -24,10 +24,10 @@ int Date::get_day() const{
 }
 
 void Date::inc_day(){
-    if( (((month == 1) || (month == 4) || (month == 6) || (month == 9) || (month == 11)) && (day == 30))
+    if( (((month == 4) || (month == 6) || (month == 9) || (month == 11)) && (day == 30))
                 || ((month == 2) && (day == 29))
                 || ((flag == 1) && (month == 2) && (day == 28))
-                || (((month == 3) || (month == 5) || (month == 7) || (month == 8) || ( month == 10)) && (day == 31)) )
+                || (month == 1) || (((month == 3) || (month == 5) || (month == 7) || (month == 8) || ( month == 10)) && (day == 31)) )
     {
         month++;
         day = 1;
