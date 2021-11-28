a097c36c1fddad798e4267f67ff05577ef324561
Shunsuke0425 <migihawkssince2000@gmail.com>
2021-11-28 11:41:56 +0900

    Modify a little bit

--- ex02/test_stream.cpp
+++ ex02/test_stream.cpp
@@ -1,6 +1,7 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <iomanip>
 #include "stack.h"
 using namespace std;
 int main(int argc, char* argv[]){
@@ -19,16 +20,10 @@ int main(int argc, char* argv[]){
         stack.push(str);
         if(max_length < str.size())max_length = str.size();
     }
-    for(int i = 0; i < max_length; i++){
-        cout << "-";
-    }
-    cout << "-" << endl;
+    std::cout << std::setfill('-') << std::setw(max_length) << "-" << std::endl;
     for(int i = 0; i < max_number; i++){
         cout << stack.top() << endl;
         stack.pop();
     }
-    for(int i = 0; i < max_length; i++){
-        cout << "-";
-    }
-    cout << "-" << endl;
+    std::cout << std::setfill('-') << std::setw(max_length) << "-" << std::endl;
 }
