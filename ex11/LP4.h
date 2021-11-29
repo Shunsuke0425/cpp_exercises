f9e55288493d044370f469ae3dcc76a4da780e57
Shunsuke0425 <migihawkssince2000@gmail.com>
2021-11-29 15:06:43 +0900

    Modify a little bit

--- ex11/LP4.h
+++ ex11/LP4.h
@@ -3,7 +3,7 @@
 namespace LP4{
     template<class Fwdlt>void sort(Fwdlt begin, Fwdlt end){
         double temp;
-        for(Fwdlt it_j = (end-1); it_j > begin; it_j--){
+        for(Fwdlt it_j = begin; it_j < (end-1); it_j++){
             for(Fwdlt it_i = begin; it_i < (end-1); it_i++){
                 if(*it_i > *(it_i + 1)){
                     temp = *it_i;
